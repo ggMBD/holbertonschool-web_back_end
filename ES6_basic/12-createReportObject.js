d3ef43dd58cea92bf28fd3e6df9f00e26ca1b00c
maouia <maouiabend@gmail.com>
2024-01-23 04:02:59 +0100

    Let's create a report object

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,5 @@
+export default function createReportObject(employeesList) {
+  const dict = { ...employeesList };
+  const getNumberOfDepartments = () => Object.keys(dict).length;
+  return { allEmployees: dict, getNumberOfDepartments };
+}
