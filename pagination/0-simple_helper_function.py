8d92b6c19225a8555da9cfbd14ac00cbe87f6d8c
maouia <maouiabend@gmail.com>
2024-01-23 02:56:48 +0100

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+"""0. Simple helper function for pagination"""
+
+
+def index_range(page, page_size):
+    """Calculate the start and end index for pagination."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size - 1
+
+    return start_index, end_index
