7fd003da1e622b30a716e3a40e1f49ce44f460d5
maouia <maouiabend@gmail.com>
2024-01-23 03:00:37 +0100

    fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@
 def index_range(page, page_size):
     """Calculate the start and end index for pagination."""
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = start_index + page_size
 
     return start_index, end_index
