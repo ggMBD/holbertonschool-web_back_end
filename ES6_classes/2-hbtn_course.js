ba648d3b5ae9f1b978d93ab81ad98c4d06cd9273
maouia <maouiabend@gmail.com>
2024-02-02 20:15:15 +0100

    fix all code

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,48 +1,46 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'name');
-    this._length = this.validateNumber(length, 'length');
-    this._students = this.validateStudentsArray(students, 'students');
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
   }
 
   set name(name) {
-    this._name = this.validateString(name, 'name');
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
-  get name() {
-    return this._name
+
+  get length() {
+    return this._length;
   }
 
   set length(length) {
-    this._length = this.validateNumber(length, 'length');
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
-  get length() {
-    return this._length
+
+  get students() {
+    return this._students;
   }
 
   set students(students) {
-    this._students = this.validateStudentsArray(students, 'students');
-  }
-  get students() {
-    return this._students
-  }
-  validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new Error(`${attribute} must be a string`);
+    if (
+      Array.isArray(students)
+        && students.every((s) => typeof s === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
-    return value;
-  }
-
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number' || isNaN(value)) {
-      throw new Error(`${attribute} must be a valid number`);
-    }
-    return value;
-  }
-
-  validateStudentsArray(value, attribute) {
-    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
-      throw new Error(`${attribute} must be an array of strings`);
-    }
-    return value;
   }
 }
