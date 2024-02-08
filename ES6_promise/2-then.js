d5bee7c66afa28828edddc78a9a8f4a7f0661a2c
maouia <maouiabend@gmail.com>
2024-02-08 03:13:54 +0100

    is ok

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,8 +2,8 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return {
-      status: 200,
       body: 'success',
+      status: 200,
     };
   }).catch(() => {
     console.log('Got a response from the API');
